fa25e74a804ae20bb64ab3a5ae376d21ae439d77
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2025-03-24 11:32:53 +0100

    pure functions

--- src/modules/sort-filter.ts
+++ src/modules/sort-filter.ts
@@ -1,11 +1,13 @@
 import { Task } from "./class";
 import { displayAllTasks } from "./display";
 
-const filterByCategory = (tasks: Task[], category: string): Task[] =>
-    category !== "all" ? tasks.filter(task => task.category === category) : tasks;
+const filterByCategory = (tasks: Task[], category: string): Task[] => {
+    return category !== "all" ? tasks.filter(task => task.category === category) : tasks.slice();
+}
 
-const filterByMember = (tasks: Task[], member: string): Task[] =>
-    member !== "all" ? tasks.filter(task => task.assigned === member) : tasks;
+const filterByMember = (tasks: Task[], member: string): Task[] => {
+    return member !== "all" ? tasks.filter(task => task.assigned === member) : tasks.slice();
+}
 
 const sortTasks = (tasks: Task[], sortBy: string): Task[] => {
     const newTasks = [...tasks];
